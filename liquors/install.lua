78bab46b4513b0e449d95beb79ddb79f0a0cb2ec
YRNBAM <145809841+bigbam974@users.noreply.github.com>
2024-10-01 17:45:26 -0400

    Add files via upload

---
+++ liquors/install.lua
@@ -0,0 +1,54 @@
+this is to just getr them into the server you have to make a shop accordingly to the script your using
+
+join discords for support
+
+                                --    ADD to    --
+-------------------------------QB/QB-Core/Shared/Items-----------------------------------
+
+
+crownapple 					= {name = 'crownapple', 			 	  	label = 'Crown Apple', 	            weight = 20, 		type = 'item', 		image = 'crownapple.png', 				unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Crown Apple Liquor'},
+twistedtea 					= {name = 'twistedtea', 			 	  	label = 'Twisted Tea', 	            weight = 20, 		type = 'item', 		image = 'twistedtea.png', 				unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Nice to eat'},
+hennessey 					= {name = 'hennessey', 			 	  	  	label = 'Hennessey', 	            weight = 20, 		type = 'item', 		image = 'hennessey.png', 				unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Nice to eat'},
+jackdaniels					= {name = 'jackdaniels', 			 	  	label = 'Jack Daniels', 	        weight = 20, 		type = 'item', 		image = 'jackdaniels.png', 				unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Nice to eat'},
+maddog2020 					= {name = 'maddog2020', 			 	  	label = 'Maddogg2020', 	            weight = 20, 		type = 'item', 		image = 'maddog2020.png', 				unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Nice to eat'},
+jamenson 					= {name = 'jamenson', 			 	  	  	label = 'Jamenson', 	            weight = 20, 		type = 'item', 		image = 'jamenson.png', 				unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Nice to eat'},
+ej 					        = {name = 'ej', 			 	  	  	    label = 'EJ', 	                    weight = 20, 		type = 'item', 		image = 'ej.png', 				        unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Nice to eat'},
+patron   					= {name = 'patron', 			 	  	  	label = 'Patron', 	                weight = 20, 		type = 'item', 		image = 'patron.png', 				    unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Nice to eat'},
+greygoose   				= {name = 'greygoose', 			 	  	  	label = 'GreyGoose', 	            weight = 20, 		type = 'item', 		image = 'greygoose.png', 				unique = false, 	useable = true, 	shouldClose = true,   combinable = nil,   description = 'Nice to eat'},
+
+
+
+                           --    ADD to    --
+--------------------------QB/QB-Smallresources/Config-------------------------------
+
+
+ ----AddedLiquor	
+ ['crownapple'] = math.random(20, 30),
+ ['twistedtea'] = math.random(30, 40),
+ ['hennessey'] = math.random(20, 40),
+ ['jackdaniels'] = math.random(20, 40),
+ ['maddog2020'] = math.random(20, 40),
+ ['jamenson'] = math.random(20, 40),
+ ['ej'] = math.random(20, 40),
+ ['patron'] = math.random(20, 40),
+ ['greygoose'] = math.random(20, 40),
+
+
+
+
+                                --    ADD to    --
+ ------------------ADD IMAGES TO QB/QB-inventory/HTML/Images--------------------------
+Enjoyyyyyyyyy
+
+___YRNBAM
+
+join my discords
+
+--Cars&Things--freecars--freemlos
+https://discord.gg/h3deXeGcBC
+
+--SDRP.V1
+https://discord.gg/f5BR4AVBev
+
+--Westpalm100kordie
+https://discord.gg/f5BR4AVBev
